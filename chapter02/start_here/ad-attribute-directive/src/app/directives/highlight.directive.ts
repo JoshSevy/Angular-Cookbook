f1ad2bdd5fbc88ab6206c261ab4ed8c5508b3a13
JoshSevy <joshuasevy@outlook.com>
2023-03-16 22:56:58 -0600

    Finish ad-attribute-directive

---
+++ chapter02/start_here/ad-attribute-directive/src/app/directives/highlight.directive.ts
@@ -0,0 +1,37 @@
+import {
+  Directive,
+  ElementRef,
+  Input,
+  OnChanges,
+  SimpleChanges,
+} from '@angular/core';
+
+@Directive({
+  selector: '[highlight]',
+})
+export class HighlightDirective implements OnChanges {
+  @Input() highlightText: string;
+  @Input() highlightColor: string = 'yellow';
+  originalHTML = '';
+
+  constructor(private el: ElementRef) {}
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes.highlightText.firstChange) {
+      this.originalHTML = this.el.nativeElement.innerHTML;
+      return;
+    }
+    const { currentValue, previousValue } = changes.highlightText;
+    if (currentValue) {
+      const regExp = new RegExp(`(${currentValue})`, 'gi');
+      this.el.nativeElement.innerHTML = this.originalHTML.replace(
+        regExp,
+        `<span style="background-color: ${this.highlightColor}">\$1</span>`
+      );
+    } else {
+      this.el.nativeElement.innerHTML = this.originalHTML;
+    }
+    console.log('currentValue', currentValue);
+    console.log('previousValue', previousValue);
+  }
+}
